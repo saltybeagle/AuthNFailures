9d964292f74148c00df6d69febdb03bed0e00224
Brett Bieber <brett.bieber@gmail.com>
2013-07-09 09:34:45 -0500

    Add the subject manager to the navigation

--- www/templates/html/AuthNFailures/Navigation.tpl.php
+++ www/templates/html/AuthNFailures/Navigation.tpl.php
@@ -3,4 +3,5 @@
     <li><a href="<?php echo $controller->getURL(); ?>manage/events">Events</a></li>
     <li><a href="<?php echo $controller->getURL(); ?>manage/resets">Resets</a>
     <li><a href="<?php echo $controller->getURL(); ?>manage/counts">Counts</a></li>
+    <li><a href="<?php echo $controller->getURL(); ?>manage/subjects">Subjects</a></li>
 </ul>
