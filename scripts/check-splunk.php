4b90cca6a9f5acfbd1c1e1162235186c76b54c35
Brett Bieber <brett.bieber@gmail.com>
2013-07-25 09:46:07 -0500

    Add sample arguments and documentation about how/when it should be run

--- scripts/check-splunk.php
+++ scripts/check-splunk.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This script is a sample splunk monitor which should be run every 10 minutes or so
+ *
+ * The searches and callback iterators should be defined in your configuration as 
+ * $splunk_searches = array([search]=>Closure)
+ *
+ * @author Brett Bieber <brett.bieber@gmail.com>
+ *
+ */
 namespace AuthNFailures;
 
 use AuthNFailures\SyslogAccumulator\SplunkMonitor;
@@ -12,6 +21,11 @@ require_once $config_file;
 
 $monitor = new SplunkMonitor($splunk_config);
 
+$args = array(
+    'earliest_time' => '-15m', // find events up to 15 minutes ago
+    'latest_time'   => 'now',  // until now
+);
+
 foreach ($splunk_searches as $searchExpression => $resultCallback) {
-    $monitor->searchAndCallback($searchExpression, $resultCallback);
+    $monitor->searchAndCallback($searchExpression, $resultCallback, $args);
 }
