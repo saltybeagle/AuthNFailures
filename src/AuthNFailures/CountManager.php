ece0521dc5d54e8b44b4ece60a1239905960f403
Brett Bieber <brett.bieber@gmail.com>
2013-07-25 08:48:47 -0500

    Add a method for determining the number of events since last reset

--- src/AuthNFailures/CountManager.php
+++ src/AuthNFailures/CountManager.php
@@ -16,4 +16,32 @@ class CountManager extends Counts
     {
         return 'ORDER BY subject ASC';
     }
+
+    /**
+     * Get the SQL for determining the total count of events since last reset.
+     *
+     * If the $subject parameter is passed, two placeholders for a prepared
+     * statement will be inserted.
+     *
+     * @param string $subject ID of the subject
+     *
+     * @return string
+     */
+    public static function getSQLForCountSinceLastReset($subject = null)
+    {
+        $sql = 'SELECT count(*) AS current_count, events.subject
+                FROM `events`
+                    LEFT JOIN ('.ResetManager::getSQLForLastReset($subject).') AS last_resets
+                        ON events.subject = last_resets.subject
+                WHERE
+                    (
+                        (events.timestamp > last_resets.last_reset)
+                        OR last_resets.last_reset IS NULL
+                    )';
+        if ($subject) {
+            $sql .= ' AND events.subject = ? ';
+        }
+        $sql .= 'GROUP BY events.subject';
+        return $sql;
+    }
 }
