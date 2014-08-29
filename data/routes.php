312a745a46e6db273e3a2edee121a3cdf055f133
Brett Bieber <brett.bieber@gmail.com>
2014-08-29 13:39:10 -0500

    Whitespace alignment and readability improvements.

--- data/routes.php
+++ data/routes.php
@@ -3,15 +3,19 @@
 /*
  * Map of regular expressions which map to models the controller will construct
  */
-$routes                                             = array();
-$routes['/^$/']                                     = 'AuthNFailures\WelcomeScreen';
-$routes['/^manage\/events(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\EventManager';
-$routes['/^manage\/counts(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\CountManager';
-$routes['/^manage\/resets(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\ResetManager';
-$routes['/^manage\/subjects(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\SubjectManager';
+$routes = array();
+
+// Allow format to be specified with a file extension, e.g. .html
+$format = '(\.(?P<format>[\w]+))?';
+
+$routes['/^$/']                            = 'AuthNFailures\WelcomeScreen';
+$routes['/^manage\/events'.$format.'$/']   = 'AuthNFailures\EventManager';
+$routes['/^manage\/counts'.$format.'$/']   = 'AuthNFailures\CountManager';
+$routes['/^manage\/resets'.$format.'$/']   = 'AuthNFailures\ResetManager';
+$routes['/^manage\/subjects'.$format.'$/'] = 'AuthNFailures\SubjectManager';
 
 
-$routes['/^subjects\/(?P<id>[\w]+)(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\Subject';
+$routes['/^subjects\/(?P<id>[\w]+)'.$format.'$/'] = 'AuthNFailures\Subject';
 
 // // List of all the active record classes
 // // [collectionurl] = array(listClass, itemClass)
@@ -22,7 +26,7 @@ $routes['/^subjects\/(?P<id>[\w]+)(\.(?P<format>[\w]+))?$/'] = 'AuthNFailures\Su
 // );
 
 // Now the fallback edit forms for most other tables
-$routes['/^(?P<table>[a-z]+)\/new$/']                                = 'AuthNFailures\ActiveRecord\Record\EditForm';
-$routes['/^(?P<table>[a-z]+)\/(?P<id>[\w]+)\/edit$/']                = 'AuthNFailures\ActiveRecord\Record\EditForm';
+$routes['/^(?P<table>[a-z]+)\/new$/']                 = 'AuthNFailures\ActiveRecord\Record\EditForm';
+$routes['/^(?P<table>[a-z]+)\/(?P<id>[\w]+)\/edit$/'] = 'AuthNFailures\ActiveRecord\Record\EditForm';
 
 return $routes;
