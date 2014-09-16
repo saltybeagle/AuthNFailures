dadc3a3b04e3fae4859e5934e290fe3e76537629
Brett Bieber <brett.bieber@gmail.com>
2014-09-16 15:44:51 -0500

    Minor improvements to the table view
    Call getURL() if the method exists
    Add classes for header and data cells

--- www/templates/html/AuthNFailures/ActiveRecord/RecordSet/Table.tpl.php
+++ www/templates/html/AuthNFailures/ActiveRecord/RecordSet/Table.tpl.php
@@ -8,6 +8,9 @@ if (!count($context)) {
 // Get table headers
 $options = $context->getDefaultOptions();
 $headers = get_class_vars($options['itemClass']);
+if (count($headers) == 0) {
+    $headers = array_fill_keys($context->getRawObject()->getColumns(), null);
+}
 
 ?>
 
@@ -15,7 +18,7 @@ $headers = get_class_vars($options['itemClass']);
     <thead>
         <tr>
             <?php foreach ($headers as $header=>$value): ?>
-            <th><?php echo $header; ?></th>
+            <th class="col_<?php echo $header; ?>"><?php echo $header; ?></th>
             <?php endforeach; ?>
         </tr>
     </thead>
@@ -25,7 +28,13 @@ $headers = get_class_vars($options['itemClass']);
             <?php
             foreach ($headers as $column=>$value) {
                 $value = nl2br($record->$column);
-                echo '<td><a href="'.$record->getURL().'/edit">' . $value . '</a></td>';
+                echo '<td class="col_'.$column.'">';
+                if (method_exists($record, 'getURL')) {
+                   echo '<a href="'.$record->getURL().'/edit">'.$value.'</a>';
+                } else {
+                   echo $value;
+                }
+                echo '</td>';
             }
             ?>
         </tr>
