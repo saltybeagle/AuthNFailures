164643a32fd0d465c42dbff0233e3321e2fa60f7
Brett Bieber <brett.bieber@gmail.com>
2015-08-10 16:28:33 -0500

    Search for password resets FIRST

--- scripts/splunk-search-definitions.php
+++ scripts/splunk-search-definitions.php
@@ -5,6 +5,40 @@ namespace AuthNFailures;
 // Set of Splunk searches and callbacks
 $splunk_searches = array();
 
+// Password changes
+$splunk_searches['search source="unl-is-idm" MOD OR RESULT | transaction conn op maxpause=35s | search "MOD attr=userPassword" err=0 | fields + uid'] = function($iterator) {
+	$result = $iterator->current();
+	try {
+
+		$time = SyslogAccumulator\SplunkMonitor::getLastSplunkIndexTimestamp($result['_indextime']);
+
+		$raw = implode($result['_raw']);
+		$raw = str_replace('\n', PHP_EOL, $raw);
+
+		$subject = new Subject();
+		$subject->setId($result['uid']);
+
+		$subject->addReset(
+				$time,      // timestamp of reset
+				sha1($raw), // unique key for this event
+				$raw        // store raw data for auditing
+		);
+
+		// If that succeeded, reset the counter
+		$subject->resetCounter($time);
+
+	} catch (ActiveRecord\Exception $exception) {
+		if (($previous = $exception->getPrevious())
+				&& (1062 == $previous->getCode())) {
+					// Duplicate key failure, this event has already been logged
+					return false;
+				}
+				// Re-throw
+				throw $exception;
+	}
+	return true;
+};
+
 // LDAP authentication failures
 $splunk_searches['search source="unl-is-idm" | transaction conn maxpause=35s | where err=49 | rex field=_raw "(?<source_ip>\d{1,3}.\d{1,3}\.\d{1,3}\.\d{1,3}(?!\d))" | fields + uid + source_ip + host'] =  function ($iterator) {
 	$result = $iterator->current();
@@ -80,40 +114,6 @@ $splunk_searches['search sourcetype="WinEventLog:Security" (EventCode="4771" AND
 		if (($previous = $exception->getPrevious())
 				&& (1062 == $previous->getCode())) {
 					// Duplicate key failure, this event has already been logged, end here we're caught up
-					return false;
-				}
-				// Re-throw
-				throw $exception;
-	}
-	return true;
-};
-
-// Password changes
-$splunk_searches['search source="unl-is-idm" MOD OR RESULT | transaction conn op maxpause=35s | search "MOD attr=userPassword" err=0 | fields + uid'] = function($iterator) {
-	$result = $iterator->current();
-	try {
-
-		$time = SyslogAccumulator\SplunkMonitor::getLastSplunkIndexTimestamp($result['_indextime']);
-
-		$raw = implode($result['_raw']);
-		$raw = str_replace('\n', PHP_EOL, $raw);
-
-		$subject = new Subject();
-		$subject->setId($result['uid']);
-
-		$subject->addReset(
-				$time,      // timestamp of reset
-				sha1($raw), // unique key for this event
-				$raw        // store raw data for auditing
-		);
-
-		// If that succeeded, reset the counter
-		$subject->resetCounter($time);
-
-	} catch (ActiveRecord\Exception $exception) {
-		if (($previous = $exception->getPrevious())
-				&& (1062 == $previous->getCode())) {
-					// Duplicate key failure, this event has already been logged
 					return false;
 				}
 				// Re-throw
